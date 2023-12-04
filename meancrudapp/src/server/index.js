452517eef821afd8fc6b2a36fd23bd40210c148d
Jayachandran Kandasamy <jayachandran.kandasamy@gmail.com>
2023-12-04 10:57:57 +0530

    commented the swagger doc section in the index.js

--- meancrudapp/src/server/index.js
+++ meancrudapp/src/server/index.js
@@ -29,35 +29,35 @@ dbConnect().then(() => {
     console.log(`DB connection error due to ${err}`);
 });
 
-const options = {
-    definition: {
-      openapi: "3.1.0",
-      info: {
-        title: "Sample Movie ticket booking",
-        version: "0.1.0",
-        description:
-          "This is a simple CRUD API application made with Express and documented with Swagger",
+// const options = {
+//     definition: {
+//       openapi: "3.1.0",
+//       info: {
+//         title: "Sample Movie ticket booking",
+//         version: "0.1.0",
+//         description:
+//           "This is a simple CRUD API application made with Express and documented with Swagger",
         
-      },
-      servers: [
-        {
-          url: "http://localhost:3000",
-        },
-      ],
-    },
-    apis: ["./router_moviebooking.js",
-    "./router_theaterSelection.js",
-    "./router_ticketbooking.js",
-    "./router_theateronboarding.js",
-    "./model/*.js"],
-  };
+//       },
+//       servers: [
+//         {
+//           url: "http://localhost:3000",
+//         },
+//       ],
+//     },
+//     apis: ["./router_moviebooking.js",
+//     "./router_theaterSelection.js",
+//     "./router_ticketbooking.js",
+//     "./router_theateronboarding.js",
+//     "./model/*.js"],
+//   };
   
-  const specs = swaggerJsdoc(options);
-  app.use(
-    "/api-docs",
-    swaggerUi.serve,
-    swaggerUi.setup(specs)
-  );
+//   const specs = swaggerJsdoc(options);
+//   app.use(
+//     "/api-docs",
+//     swaggerUi.serve,
+//     swaggerUi.setup(specs)
+//   );
 
 
 app.listen(PORT, () => {
