1e022cfd88628a4017ecbf8cb817f9b857007a25
Jayachandran Kandasamy <jayachandran.kandasamy@gmail.com>
2023-12-04 11:34:40 +0530

    revereted the changes in the index.js

--- meancrudapp/src/server/index.js
+++ meancrudapp/src/server/index.js
@@ -13,14 +13,14 @@ const theaterOnboarding = require('./router_theateronboarding.js');
 const swaggerJsdoc = require("swagger-jsdoc");
 const swaggerUi = require("swagger-ui-express");
 
-app.use(cors());
+app.use(cors({}));
 app.use(bodyParser.json());
 app.use(express.json());
 // app.use('/api/employee', emproutes);
-app.use('/', cityroutes);
-app.use('/', theaterSelection);
-app.use('/', bookingroutes); 
-app.use('/', theaterOnboarding); 
+app.use('/api/cities', cityroutes);
+app.use('/api/theaters', theaterSelection);
+app.use('/api/booking', bookingroutes); 
+app.use('/api/theaterboarding', theaterOnboarding); 
 
 dbConnect().then(() => {
     console.log('DB connected successfully!!');
@@ -29,35 +29,35 @@ dbConnect().then(() => {
     console.log(`DB connection error due to ${err}`);
 });
 
-// const options = {
-//     definition: {
-//       openapi: "3.1.0",
-//       info: {
-//         title: "Sample Movie ticket booking",
-//         version: "0.1.0",
-//         description:
-//           "This is a simple CRUD API application made with Express and documented with Swagger",
+const options = {
+    definition: {
+      openapi: "3.1.0",
+      info: {
+        title: "Sample Movie ticket booking",
+        version: "0.1.0",
+        description:
+          "This is a simple CRUD API application made with Express and documented with Swagger",
         
-//       },
-//       servers: [
-//         {
-//           url: "http://localhost:3000",
-//         },
-//       ],
-//     },
-//     apis: ["./router_moviebooking.js",
-//     "./router_theaterSelection.js",
-//     "./router_ticketbooking.js",
-//     "./router_theateronboarding.js",
-//     "./model/*.js"],
-//   };
+      },
+      servers: [
+        {
+          url: "http://localhost:3000",
+        },
+      ],
+    },
+    apis: ["./router_moviebooking.js",
+    "./router_theaterSelection.js",
+    "./router_ticketbooking.js",
+    "./router_theateronboarding.js",
+    "./model/*.js"],
+  };
   
-//   const specs = swaggerJsdoc(options);
-//   app.use(
-//     "/api-docs",
-//     swaggerUi.serve,
-//     swaggerUi.setup(specs)
-//   );
+  const specs = swaggerJsdoc(options);
+  app.use(
+    "/api-docs",
+    swaggerUi.serve,
+    swaggerUi.setup(specs)
+  );
 
 
 app.listen(PORT, () => {
